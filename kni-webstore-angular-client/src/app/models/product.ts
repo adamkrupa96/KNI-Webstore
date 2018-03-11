31c7b21a23a90e83783e5aa03c61824aaf8b4f33
adamkrupa96 <adamkrupa96@gmail.com>
2018-03-12 00:00:19 +0100

    fix product model

--- kni-webstore-angular-client/src/app/models/product.ts
+++ kni-webstore-angular-client/src/app/models/product.ts
@@ -2,14 +2,14 @@ import { Feature } from './feature';
 
 export class Product {
   constructor(
-    public id: number,
-    public name: string,
-    public shortDescription: string,
-    public longDescription: string,
-    public brand: string,
-    public model: string,
-    public price: number,
-    public inStock: number,
-    public features: Feature[],
+    public id?: number,
+    public name?: string,
+    public shortDescription?: string,
+    public longDescription?: string,
+    public brand?: string,
+    public model?: string,
+    public price?: number,
+    public inStock?: number,
+    public features?: Feature[],
   ) {}
 }
