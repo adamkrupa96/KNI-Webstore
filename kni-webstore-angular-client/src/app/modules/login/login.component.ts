087838e8d2f3e3814cb08caaae57b52aa9d0446d
Adam Krupa <adamkrupa96@gmail.com>
2018-05-03 15:36:47 +0200

    bind user object with form fields.

--- kni-webstore-angular-client/src/app/modules/login/login.component.ts
+++ kni-webstore-angular-client/src/app/modules/login/login.component.ts
@@ -12,6 +12,7 @@ import { User } from '../../models/user';
 export class LoginComponent implements OnInit {
   loading = false;
   error = '';
+  user: User;
 
   loginForm: FormGroup;
 
@@ -25,27 +26,20 @@ export class LoginComponent implements OnInit {
   }
 
   createLoginForm() {
+    this.user = new User();
     this.loginForm = new FormGroup({
-      username: new FormControl(null, [Validators.required, Validators.minLength(4), Validators.maxLength(50)]),
-      password: new FormControl(null, [Validators.required, Validators.minLength(4), Validators.maxLength(50)])
+      username: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(50)]),
+      password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(50)])
     });
   }
 
   login() {
     this.loading = true;
-    this.authService.login(this.loginForm.value.username, this.loginForm.value.password)
+    this.authService.login(this.user.username, this.user.password)
       .subscribe(result => {
         if (result === true) {
-          // login successful
-          // przejscie na strone przed ktora bylismy zanim sie zalogowalismy
-          const redirectUrl = this.authService.getRedirectUrl();
-          console.log(redirectUrl);
-          if (redirectUrl) {
-            this.router.navigate([redirectUrl]);
-            this.authService.setRedirectUrl(null);
-          } else if (redirectUrl === undefined || redirectUrl === null) {
-            this.router.navigate(['/home']);
-          }
+          this.authService.redirectToPreviousPage();
+          this.createLoginForm();
         }
       }, error => {
         this.loading = false;
