0776271baf160607a202d937c70aefde3344aaf4
Adam Krupa <adamkrupa96@gmail.com>
2018-05-03 13:37:01 +0200

    clean up imports in routing.module

--- kni-webstore-angular-client/src/app/routing.module.ts
+++ kni-webstore-angular-client/src/app/routing.module.ts
@@ -3,12 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './modules/home/home.component';
 import { RegisterComponent } from './modules/register/register.component';
 import { LoginComponent } from './modules/login/login.component';
-import { AuthGuardsService } from './services/auth-guards.service';
-import { AdminPanelComponent } from './modules/admin-panel/admin-panel.component';
-import { OfferComponent } from './modules/offer/offer.component';
-import { CategoriesComponent } from './modules/offer/components/categories/categories.component';
-import { ProductsOfCategoryComponent } from './modules/offer/components/products-of-category/products-of-category.component';
-import { ProductPageComponent } from './modules/offer/components/product-page/product-page.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
