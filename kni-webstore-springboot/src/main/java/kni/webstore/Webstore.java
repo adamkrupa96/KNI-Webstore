7b9c95be9d0acd409b0cc9672e0ea78aa215209b
adam <adamkrupa96@gmail.com>
2018-05-07 20:38:13 +0200

    init storage for upload files after start application

--- kni-webstore-springboot/src/main/java/kni/webstore/Webstore.java
+++ kni-webstore-springboot/src/main/java/kni/webstore/Webstore.java
@@ -1,12 +1,26 @@
 package kni.webstore;
 
+import javax.annotation.Resource;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import kni.webstore.service.UploadFilesService;
+
 @SpringBootApplication
-public class Webstore {
+public class Webstore implements CommandLineRunner {
+	
+	@Resource
+	UploadFilesService uploadFilesService;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Webstore.class, args);
+	}
+	
+	@Override
+	public void run(String... arg) throws Exception {
+		uploadFilesService.deleteAllFilesFromStorage();
+		uploadFilesService.initStorageForFiles();
 	}
 }
