92692af605aa869f89d42c4d107b860d1d902e95
Kijko <kijkongs@gmail.com>
2018-05-21 15:04:32 +0200

    offer as home page

--- kni-webstore-angular-client/src/app/routing.module.ts
+++ kni-webstore-angular-client/src/app/routing.module.ts
@@ -3,10 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './modules/home/home.component';
 import { RegisterComponent } from './modules/register/register.component';
 import { LoginComponent } from './modules/login/login.component';
+import { OfferComponent } from './modules/offer';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/offer', pathMatch: 'full' },
+  { path: 'home', redirectTo: '/offer', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: '**', component: HomeComponent }
